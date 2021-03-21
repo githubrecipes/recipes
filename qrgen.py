5e8192a6c9af48ec12020c2b34c26068b605c714
Sam Sebastian <sebastiansam55@gmail.com>
2021-03-21 17:28:03 -0400

    Add qrgen.py script

---
+++ qrgen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import qrcode
+import sys
+
+img = qrcode.make(sys.argv[1])
+title = sys.argv[2].lower().replace(" ", "-")
+file = title+".png"
+img.save("./assets/"+file)
+
+print("""
+<div class=\"qrcode\">
+    <img src=\"https://githubrecipes.com/assets/{}\" alt=\"{}\" width=\"300\" height=\"300\">
+</div>
+""".format(file, title))
+
